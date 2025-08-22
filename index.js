c1eaab2bcb8c266708c0ec44724607a878383cef
shreyventure <shreyas.shrawage2000@gmail.com>
2025-08-22 15:29:55 -0700

    [Issue-27] Show message for contribution and bug report

--- index.js
+++ index.js
@@ -99,4 +99,7 @@ import { createAxiosSetup, createAppComponent, setupRouterMain, createPWAReadme 
     if (isPWA) {
         console.log(`\n📱 To test PWA:\n  npm run build\n  npm run preview\n  Open http://localhost:5173 and test install/offline features`);
     }
+    const githubRepo = "https://github.com/harshgupta20/quickstart-react/issues"; 
+    console.log(`\n🙌 Found a bug or want to improve this project?\nSubmit a PR or open an issue here: ${githubRepo}\n`);
+    console.log("\nHappy coding! 🎉");
 })();
